d6d5d2f9da2a39777d5c333cd9bf5b2a19ade0a5
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-01-29 18:02:51 +0300

    feat : enable cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 var database = require("./config/db");
 
 var indexRouter = require("./routes/index");
@@ -13,6 +14,11 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
